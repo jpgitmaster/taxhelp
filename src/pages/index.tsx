f7dbe6814122896e0176cf8cb9b79c9a5274740f
John Patrick Vasquez <jpvasquez@Johns-MacBook-Pro.local>
2026-03-28 22:46:44 +0800

    updated sliding banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,7 @@ export default function LandingPage() {
                 </Link>
               </div>
               <div className={scss.bannerRight}>
-                <img src="https://images.unsplash.com/photo-1642043175009-5997b3a078d8?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Accounting Team" />
+                <img src="https://plus.unsplash.com/premium_photo-1679923906285-386991e8d862?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Accounting Team" />
               </div>
             </div>
             <div className={scss.banner}>
@@ -87,7 +87,7 @@ export default function LandingPage() {
                 </Link>
               </div>
               <div className={scss.bannerRight}>
-                <img src="https://plus.unsplash.com/premium_photo-1661761077411-d50cba031848?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Accounting Team" />
+                <img src="https://plus.unsplash.com/premium_photo-1679922389798-8b38c78b5670?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Accounting Team" />
               </div>
             </div>
             <div className={scss.banner}>
