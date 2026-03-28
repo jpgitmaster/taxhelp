0aa1ee9e0477d85833abb0b06e92f6b5e089de1c
John Patrick Vasquez <jpvasquez@Johns-MacBook-Pro.local>
2026-03-28 16:12:16 +0800

    updates in login and sliding banners

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,7 @@ export default function LandingPage() {
                 </Link>
               </div>
               <div className={scss.bannerRight}>
-                <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&w=600&q=80" alt="Accounting Team" />
+                <img src="https://images.unsplash.com/photo-1642043175009-5997b3a078d8?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Accounting Team" />
               </div>
             </div>
             <div className={scss.banner}>
