e19aafc9b1850b2f03963061629262e6ec2b1455
John Patrick Vasquez <jpvasquez@Johns-MacBook-Pro.local>
2026-03-29 08:44:44 +0800

    adding forgot password form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -145,7 +145,7 @@ export default function LandingPage() {
         {/* About Section */}
         <section className={scss.about}>
             <div className={scss.about_box}>
-                <Image src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?auto=format&fit=crop&w=400&q=80" alt="About Us" className={scss.about_img} width={20} height={20} unoptimized={true} />
+                <Image src="https://images.unsplash.com/photo-1642043175009-5997b3a078d8?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="About Us" className={scss.about_img} width={20} height={20} unoptimized={true} />
                 <div>
                     <h2>About TaxHelp</h2>
                     <p>
