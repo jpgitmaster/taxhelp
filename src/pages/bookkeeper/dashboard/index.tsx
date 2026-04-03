3864e0e8571ba6adf76828b2917bde1af70de785
John Patrick Vasquez <jpvasquez@Johns-MacBook-Pro.local>
2026-04-03 21:02:21 +0800

    updates in clients responsive

--- src/pages/bookkeeper/dashboard/index.tsx
+++ src/pages/bookkeeper/dashboard/index.tsx
@@ -14,7 +14,7 @@ const Dashboard_V = () => {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 800) {
-        setCalendarHeight(350); // mobile → scroll
+        setCalendarHeight(320); // mobile → scroll
       }
     };
 
